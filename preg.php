e8820be01e748ebed88a52a4d674c11b70b5325b
Naina <nainasaini222@gmail.com>
2018-03-23 12:14:12 +0530

    Create preg.php

---
+++ preg.php
@@ -0,0 +1,318 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta tags -->
+	<title>Daily Application Form </title>
+	<meta name="keywords" content="General Application Form Responsive widget, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" href="preg.css" type="text/css" media="all">
+	
+	<!-- google fonts  -->
+	<link href="//fonts.googleapis.com/css?family=Alegreya+Sans:100,100i,300,300i,400,400i,500,500i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+
+</head>
+<body  background="f7499c1d692de2f8fab3f42e5e4e6fd4.jpg"  background="no-repeat" background="center">
+	<div class="w3ls-banner">
+	<div class="heading">
+		<h1>Daily Application Form</h1>
+	</div>
+		<div class="container">
+			<div class="heading">
+				<h2>Please Enter Your Details</h2>
+				<p>Fill the application form below and submit.</p>
+			</div>
+			<div class="agile-form">
+				<form action="#" method="post">
+					<ul class="field-list">
+						<li class="name">
+							<label class="form-label"> Name <span class="form-required"> * </span></label>
+							<div class="form-input add">
+								<span class="form-sub-label">
+									<input type="text" name="firstname" placeholder="First Name" required>
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="lastname" placeholder="Last Name" required>
+								</span>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label"> E-Mail Address <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<input type="email" name="email" placeholder="Mail@example.com" >
+							
+							</div>
+						</li>
+						<li> 
+							<label class="form-label"> Adhaar number <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<input type="Text" name="Adhaar number" placeholder="Enter number" required>
+							
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Skills <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="Skills" required>
+									<option value="">Skills</option>
+                                    <option value="Watchman">Watchman </option>
+                                    <option value="House Keeper">House Keeper </option>
+                                    <option value="Farmer"> Farmer </option>
+                                    <option value="Worker"> Worker </option>
+                                    <option value="Driver">Driver </option>
+                                    <option value="Peon"> Peon </option>
+                                    <option value="Cleaner"> Cleaner </option>
+                                    <option value="Mechanic"> Mechanic </option>
+                                    <option value="Electrician"> Electrician </option>
+									<option value="Plumbing"> Plumbing </option>
+									<option value="Carpenter"> Carpenter </option>
+									<option value="Cobbler"> Cobbler </option>
+									<option value="Wall painter"> Wall painter </option>
+									<option value="Electrician"> Electrician </option>
+									<option value="Others"> Others </option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Work Area <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="Work Area" required>
+									<option value="">Work Area</option>
+									<option value="Transport"> Transport </option>
+									<option value="Construction"> Construction </option>
+									<option value="Industry"> Industry </option>
+                                    <option value="Farm">Farm </option>
+									<option value="House"> House </option>
+									<option value="Office"> Office </option>
+                                    <option value="Hospital"> Hospital </option>
+                                    <option value="College"> College </option>
+									<option value="Others"> Others </option>
+								</select>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label"> Contact <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<input type="text" name="mobile_number" placeholder="Phone Number" required >
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Gender <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="gender" required>
+									<option value="">Gender</option>
+									<option value="Male"> Male </option>
+									<option value="Female"> Female </option>
+									<option value="Transgender"> Transgender </option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> City <span class="form-required"> * </span></label>
+							<div class="form-input">
+								<select class="form-dropdown" name="city" required>
+									<option value="">City</option>
+                                    <option value="Ahmedabad"> Ahmedabad</option>
+                                    <option value="Amreli"> Amreli</option>
+                                    <option value="Anand"> Anand</option>
+                                    <option value="Aravali">Aravali </option>
+                                    <option value="Banaskantha"> Banaskantha</option>
+                                    <option value="Bharuch"> Bharuch</option>
+                                    <option value="Bhavnagar"> Bhavnagar</option>
+                                    <option value="Botad"> Botad</option>
+                                    <option value="Chhota Udaipur"> Chhota Udaipur</option>
+                                    <option value="Devbhoomi Dwarka"> Devbhoomi Dwarka</option>
+                                    <option value="Dohad"> Dohad</option>
+                                    <option value="Gandhinagar"> Gandhinagar</option>
+                                    <option value="Gir Somnath"> Gir Somnath</option>
+                                    <option value="Jamnagar"> Jamnagar</option>
+                                    <option value="Junagadh"> Junagadh</option>
+                                    <option value="Kachchh"> Kachchh</option>
+                                    <option value="Kheda">Kheda</option>
+									<option value="Mahisagar"> Mahisagar</option>
+									<option value="Mehsana"> Mehsana </option>
+                                    <option value="Morbi"> Morbi </option>
+                                    <option value="Narmada"> Narmada </option>
+                                    <option value="Navsari"> Navsari </option>
+                                    <option value="PanchMahal"> PanchMahal </option>
+                                    <option value="Patan"> Patan </option>
+                                    <option value="Porbandar"> Porbandar</option>
+                                    <option value="Rajkot"> Rajkot </option>
+                                    <option value="Sabarkantha"> Sabarkantha </option>
+                                    <option value="Surat"> Surat </option>
+                                    <option value="Surendranagar"> Surendranagar</option>
+									<option value="Tapi"> Tapi </option>
+									<option value="The Dangs"> The Dangs</option>
+									<option value="Vadodara"> Vadodara</option>
+									<option value="Valsad"> Valsad</option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<label class="form-label"> Date of Birth <span class="form-required"> * </span></label>
+							<div class="form-input dob">
+								<span class="form-sub-label">
+									<select name="day" class="day" required>
+										<option>Day</option>
+										<option value="1"> 1 </option>
+										<option value="2"> 2 </option>
+										<option value="3"> 3 </option>
+										<option value="4"> 4 </option>
+										<option value="5"> 5 </option>
+										<option value="6"> 6 </option>
+										<option value="7"> 7 </option>
+										<option value="8"> 8 </option>
+										<option value="9"> 9 </option>
+										<option value="10"> 10 </option>
+										<option value="11"> 11 </option>
+										<option value="12"> 12 </option>
+										<option value="13"> 13 </option>
+										<option value="14"> 14 </option>
+										<option value="15"> 15 </option>
+										<option value="16"> 16 </option>
+										<option value="17"> 17 </option>
+										<option value="18"> 18 </option>
+										<option value="19"> 19 </option>
+										<option value="20"> 20 </option>
+										<option value="21"> 21 </option>
+										<option value="22"> 22 </option>
+										<option value="23"> 23 </option>
+										<option value="24"> 24 </option>
+										<option value="25"> 25 </option>
+										<option value="26"> 26 </option>
+										<option value="27"> 27 </option>
+										<option value="28"> 28 </option>
+										<option value="29"> 29 </option>
+										<option value="30"> 30 </option>
+										<option value="31"> 31 </option>
+									</select>
+								</span>
+								<span class="form-sub-label">
+									<select name="month" required>
+										<option>Month</option>
+										<option value="January"> January </option>
+										<option value="February"> February </option>
+										<option value="March"> March </option>
+										<option value="April"> April </option>
+										<option value="May"> May </option>
+										<option value="June"> June </option>
+										<option value="July"> July </option>
+										<option value="August"> August </option>
+										<option value="September"> September </option>
+										<option value="October"> October </option>
+										<option value="November"> November </option>
+										<option value="December"> December </option>
+									 </select>
+								</span>
+								<span class="form-sub-label">
+									<select name="year" required>
+										<option>Year</option>
+										<option value="1990"> 1990 </option>
+										<option value="1990"> 1991 </option>
+										<option value="1990"> 1992 </option>
+										<option value="1990"> 1993 </option>
+										<option value="1990"> 1994 </option>
+										<option value="1990"> 1995 </option>
+										<option value="1990"> 1996 </option>
+										<option value="1990"> 1997 </option>
+										<option value="1990"> 1998 </option>
+										<option value="1990"> 1999 </option>
+										<option value="1990"> 2000 </option>
+										<option value="1990"> 2001 </option>
+										<option value="1990"> 2002 </option>
+										<option value="1990"> 2003 </option>
+										<option value="1990"> 2004 </option>
+										<option value="1990"> 2005 </option>
+										<option value="1990"> 2006 </option>
+										<option value="1990"> 2007 </option>
+										<option value="1990"> 2008 </option>
+										<option value="1990"> 2009 </option>
+										<option value="1990"> 2010 </option>
+										<option value="1990"> 2011 </option>
+										<option value="1990"> 2012 </option>
+										<option value="1990"> 2013 </option>
+										<option value="1990"> 2014 </option>
+										<option value="1990"> 2015 </option>
+										<option value="1990"> 2016 </option>
+										<option value="1990"> 2017 </option>
+										<option value="1990"> 2018 </option>
+									 </select>
+								</span>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label">
+							  Permenent Address
+							   <span class="form-required">  </span>
+							</label>
+							<div class="form-input add">
+								<span class="form-sub-label">
+									<input type="text" name="street_address" placeholder="Street Address ">
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="street_address2" placeholder="Street Line 2 ">
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="landmark" placeholder="Landmark ">
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="City" placeholder="City">
+								</span>
+								<span class="form-sub-label last">
+									<input type="text" name="state" placeholder="State / Province ">
+								</span>
+								<span class="form-sub-label last">
+									<input type="text" name="code" placeholder="Postal / Zip Code ">
+								</span>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label">
+							  Temporary Address
+							   <span class="form-required"> * </span>
+							</label>
+							<div class="form-input add">
+								<span class="form-sub-label">
+									<input type="text" name="street_address" placeholder="Street Address " required>
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="street_address2" placeholder="Street Line 2 " required>
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="landmark" placeholder="Landmark " required>
+								</span>
+								<span class="form-sub-label">
+									<input type="text" name="City" placeholder="City" required>
+								</span>
+								<span class="form-sub-label last">
+									<input type="text" name="state" placeholder="State / Province " required>
+								</span>
+								<span class="form-sub-label last">
+									<input type="text" name="code" placeholder="Postal / Zip Code " required>
+								</span>
+							</div>
+						</li>
+						<li> 
+							<label class="form-label"> Certificate(If Any) <span class="form-required">  </span></label>
+							<div class="form-input">
+								<input type="Text" name="Certificate" >
+								<input type="file" id="myFile">
+							</div>
+						</li>
+					</ul>
+					
+					<div class="submit_btn">
+						<input type="submit" value="Submit">
+					</div>
+				</form>	
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
